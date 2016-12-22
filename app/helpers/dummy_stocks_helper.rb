9dbd8d3660775e4346e32515bbd2e46004c89c74
YasuAida <kirakunakaishain@yahoo.co.jp>
2016-12-22 05:59:45 +0000

    stockledger finally completed

--- app/helpers/dummy_stocks_helper.rb
+++ app/helpers/dummy_stocks_helper.rb
@@ -7,6 +7,7 @@ module DummyStocksHelper
     sale_pladmin=Pladmin.find_by(date: dummy_stock.sale_date, sku: @base_stock.sku)
     if sale_pladmin.present?
       @sale_stockledger = Stockledger.create(stock_id: dummy_stock.stock_id, transaction_date: sale_pladmin.date,sku: sale_pladmin.sku, asin: @base_stock.asin, goods_name: sale_pladmin.goods_name, classification: "販売", number: dummy_stock.number * -1, unit_price: price_unit, grandtotal: price_unit * dummy_stock.number * -1)
+      sale_pladmin.stock_id = dummy_stock.stock_id
       sale_pladmin.cgs_amount = price_unit * dummy_stock.number
       sale_pladmin.save
     else
@@ -19,6 +20,7 @@ module DummyStocksHelper
       if @cancel_stockledger.present?
         @cancel_stockledger.update(stock_id: dummy_stock.stock_id, transaction_date: cancel_pladmin.date,sku: cancel_pladmin.sku, asin: @base_stock.asin, goods_name: cancel_pladmin.goods_name, classification: "キャンセル", number: dummy_stock.number, unit_price: price_unit, grandtotal: price_unit * dummy_stock.number)
       end
+      cancel_pladmin.stock_id = dummy_stock.stock_id
       cancel_pladmin.cgs_amount = price_unit * dummy_stock.number * -1
       cancel_pladmin.save
     else
@@ -26,8 +28,13 @@ module DummyStocksHelper
     end
     
     stock_fraction = @base_stock.stockledgers.sum(:grandtotal)
-    new_grandtotal = @base_stock.stockledgers.where.not(classification: "購入").order(:transaction_date).last.grandtotal - stock_fraction
-    target_stockledger = @base_stock.stockledgers.where.not(classification: "購入").order(:transaction_date).last
+    target_stockledgers = @base_stock.stockledgers.where.not(classification: "購入")
+    target_stockledgers = target_stockledgers.where.not(classification: "返還")
+    target_stockledgers = target_stockledgers.where.not(classification: "SKU付替")
+    target_stockledgers = target_stockledgers.where.not(classification: "廃棄")
+    target_stockledgers = target_stockledgers.where.not(classification: "仕入返品")
+    target_stockledger = target_stockledgers.order(:transaction_date).last
+    new_grandtotal = target_stockledger.grandtotal - stock_fraction
     target_stockledger.grandtotal = new_grandtotal
     target_stockledger.save
     
--- app/helpers/stockledgers_helper.rb
+++ app/helpers/stockledgers_helper.rb
@@ -174,7 +174,7 @@ module StockledgersHelper
         #@sku_stocksが複数で、pladmin.sale_amountがマイナス              
           elsif pladmin.sale_amount.present? && pladmin.sale_amount < 0
             target_stocks = @sku_stocks.where("sold_unit >= ?", 1)
-            if sku_stock.soldout_check = false || target_stocks.present? && sku_stock == target_stocks.last           
+            if target_stocks.present? && sku_stock == target_stocks.last           
               @stockledger = Stockledger.create(stock_id: sku_stock.id, transaction_date: pladmin.date,sku: pladmin.sku, asin: sku_stock.asin, goods_name: pladmin.goods_name, classification: "キャンセル", number: pladmin.quantity, unit_price: price_unit, grandtotal: price_unit * pladmin.quantity)
               pladmin.stock_id = sku_stock.id              
               pladmin.cgs_amount = price_unit * pladmin.quantity * -1
@@ -213,7 +213,7 @@ module StockledgersHelper
             
         #@sku_stocksが複数で、pladmin.sale_amountがマイナスで、return_goodsが一つある  
             return_goods = ReturnGood.where(old_sku: pladmin.sku)
-            if return_goods.present? && return_goods.count == 1
+            if return_goods.present? && return_goods.count == 1 && target_stocks.present? && sku_stock == target_stocks.last
               @old_stockledger = Stockledger.create(stock_id: sku_stock.id, transaction_date: return_goods.first.date,sku: return_goods.first.old_sku, asin: sku_stock.asin, goods_name: sku_stock.goods_name, classification: "返還", number: return_goods.first.number * -1, unit_price: price_unit, grandtotal: price_unit * return_goods.first.number * -1)
               @new_stockledger = Stockledger.create(stock_id: sku_stock.id, transaction_date: return_goods.first.date,sku: return_goods.first.new_sku, asin: sku_stock.asin, goods_name: sku_stock.goods_name, classification: "SKU付替", number: return_goods.first.number, unit_price: price_unit, grandtotal: price_unit * return_goods.first.number)
               return_goods.first.stock_id = sku_stock.id
@@ -242,7 +242,7 @@ module StockledgersHelper
               end
               
         #@sku_stocksが複数で、pladmin.sale_amountがマイナスで、return_goodsが複数ある 
-            else return_goods.present? && return_goods.count > 1 
+            elsif return_goods.present? && return_goods.count > 1 && target_stocks.present? && sku_stock == target_stocks.last 
               return_goods.each do |return_good|
                 @old_stockledger = Stockledger.create(stock_id: sku_stock.id, transaction_date: return_good.date,sku: return_good.old_sku, asin: sku_stock.asin, goods_name: sku_stock.goods_name, classification: "返還", number: return_good.number * -1, unit_price: price_unit, grandtotal: price_unit * return_good.number * -1)
                 @new_stockledger = Stockledger.create(stock_id: sku_stock.id, transaction_date: return_good.date,sku: return_good.new_sku, asin: sku_stock.asin, goods_name: sku_stock.goods_name, classification: "SKU付替", number: return_good.number, unit_price: price_unit, grandtotal: price_unit * return_good.number)
@@ -409,8 +409,13 @@ module StockledgersHelper
     @stocks.each do |stock|
       if stock.stockledgers.sum(:number) == 0 && stock.stockledgers.sum(:grandtotal) != 0
         stock_fraction = stock.stockledgers.sum(:grandtotal)
-        new_grandtotal = stock.stockledgers.where.not(classification: "購入").order(:transaction_date).last.grandtotal - stock_fraction
-        target_stockledger = stock.stockledgers.where.not(classification: "購入").order(:transaction_date).last
+        target_stockledgers = stock.stockledgers.where.not(classification: "購入")
+        target_stockledgers = target_stockledgers.where.not(classification: "返還")
+        target_stockledgers = target_stockledgers.where.not(classification: "SKU付替")
+        target_stockledgers = target_stockledgers.where.not(classification: "廃棄")
+        target_stockledgers = target_stockledgers.where.not(classification: "仕入返品")
+        target_stockledger = target_stockledgers.order(:transaction_date).last
+        new_grandtotal = target_stockledger.grandtotal - stock_fraction
         target_stockledger.grandtotal = new_grandtotal
         target_stockledger.save
         
